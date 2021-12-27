156d7cb3c69233b66484b3d93e1f113e3a46f988
andy <admazzola@gmail.com>
2021-12-27 03:46:14 -0500

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -8,11 +8,13 @@
           <h3 class="heading-3 footer font-fun">Documentation</h3>
  
 
-          <a href="https://github.com/admazzola/toadz-market-web" target="_blank" class="footer-link hidden ">Open Source Code</a>
-
+        
           <a href="https://etherscan.io/address/0x0419732028fa499200a0e36972e4c139e98d28e1" target="_blank" class="footer-link">Smart Contract</a>
 
           <a href="https://docs.swampz.io" target="_blank" class="footer-link">Official Docs</a>
+
+            <a href="https://github.com/admazzola/toadz-market-web" target="_blank" class="footer-link hidden ">Open Source Code</a>
+
 
 
           </div>
@@ -23,17 +25,8 @@
 
             <a href="https://discord.gg/mUPRJbpMsG" target="_blank" class="footer-link">Swampz Discord</a>
 
-        <div class="column-7 w-col w-col-4 w-col-small-4 flex flex-col h-full">
-
-             </div>
-             
-          <div class="flex-grow h-32">
-          </div>
-
-
-          <div class="text-yellow-400">
-            
-         </div>
+           
+          
 
          </div>
       </div>

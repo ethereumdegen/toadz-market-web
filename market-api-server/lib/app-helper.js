89dd4d42264494c53b5ad96eab7ab3d1d119d50f
andy <admazzola@gmail.com>
2021-12-27 15:07:35 -0500

    fix app helper

--- market-api-server/lib/app-helper.js
+++ market-api-server/lib/app-helper.js
@@ -1,7 +1,13 @@
- 
+            import FileHelper from './file-helper.js'
+
+
     import web3utils from 'web3-utils'
     import ObjectId from 'mongodb';
  
+
+    const envmode = process.env.NODE_ENV
+   const contractData = FileHelper.readJSONFile('./market-api-server/config/contractdata.json')
+         
     export default class AppHelper  {
     
       
@@ -13,8 +19,28 @@
 
          //fix me !! convert nftContractAddress
         static contractAddressToCollectionName(contractAddress){
+
+             let networkName = AppHelper.getNetworkName() 
+
+            let contractDataForNetwork = contractData[networkName].contracts
+
+            for(let [contractName,data] of Object.entries(contractDataForNetwork)){
+                let address = data.address
+
+                if( contractAddress.toLowerCase() == address.toLowerCase()   ){
+                    return data.name
+                } 
+            } 
+
             return 'Cryptoadz'
         }  
+
+        static getNetworkName(){
+
+            if(envmode == 'production') return 'mainnet'
+
+            return 'rinkeby'
+        }
         
         
         static mongoIdToNumber(mongoId){
